e05e7547424647eee131f83fd566a93ee7307182
cesarsouza <cesarsouza@0b1c26a9-55f7-2bfe-8466-e4ca5f927057>
2012-07-08 19:34:00 +0000

    Updating version numbers.

--- trunk/Sources/Accord.Audio/Properties/AssemblyInfo.cs
+++ trunk/Sources/Accord.Audio/Properties/AssemblyInfo.cs
@@ -26,6 +26,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.6.1.0")]
-[assembly: AssemblyFileVersionAttribute("2.6.1.0")]
+[assembly: AssemblyVersion("2.7.0.0")]
+[assembly: AssemblyFileVersionAttribute("2.7.0.0")]
 
--- trunk/Sources/Accord.Audition/Properties/AssemblyInfo.cs
+++ trunk/Sources/Accord.Audition/Properties/AssemblyInfo.cs
@@ -28,8 +28,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("2.6.1.0")]
-[assembly: AssemblyFileVersionAttribute("2.6.1.0")]
+[assembly: AssemblyVersion("2.7.0.0")]
+[assembly: AssemblyFileVersionAttribute("2.7.0.0")]
 
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Accord.Audition.Beat")]
