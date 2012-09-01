c9b63c9a3ef215a89860e7845719b65b37b1d892
cesarsouza <cesarsouza@0b1c26a9-55f7-2bfe-8466-e4ca5f927057>
2012-09-01 22:32:43 +0000

    Adding documentation examples in Sequential Minimal Optimization for Regression.

--- trunk/Sources/Accord.MachineLearning.GPL/SequentialMinimalOptimizationRegression.cs
+++ trunk/Sources/Accord.MachineLearning.GPL/SequentialMinimalOptimizationRegression.cs
@@ -76,6 +76,42 @@ namespace Accord.MachineLearning.VectorMachines.Learning
     ///   </list></para>
     /// </remarks>
     /// 
+    /// <example>
+    /// <code>
+    /// // Example regression problem. Suppose we are trying
+    /// // to model the following equation: f(x, y) = 2x + y
+    /// 
+    /// double[][] inputs = // (x, y)
+    /// {
+    ///     new double[] { 0,  1 }, // 2*0 + 1 =  1
+    ///     new double[] { 4,  3 }, // 2*4 + 3 = 11
+    ///     new double[] { 8, -8 }, // 2*8 - 8 =  8
+    ///     new double[] { 2,  2 }, // 2*2 + 2 =  6
+    ///     new double[] { 6,  1 }, // 2*6 + 1 = 13
+    ///     new double[] { 5,  4 }, // 2*5 + 4 = 14
+    ///     new double[] { 9,  1 }, // 2*9 + 1 = 20
+    ///     new double[] { 1,  6 }, // 2*1 + 6 =  5
+    /// };
+        /// 
+    /// double[] outputs = // f(x, y)
+    /// {
+    ///         1, 11, 8, 6, 13, 14, 20, 5
+    /// };
+    /// 
+    /// // Create Kernel Support Vector Machine with a Polynomial Kernel of 2nd degree
+    /// var machine = new KernelSupportVectorMachine(new Polynomial(2), inputs: 2);
+    /// 
+    /// // Create the sequential minimal optimization teacher
+    /// var learn = new SequentialMinimalOptimizationRegression(machine, inputs, outputs);
+    /// 
+    /// // Run the learning algorithm
+    /// double error = learn.Run();
+    /// 
+    /// // Compute the answer for one particular example
+    /// double fxy = machine.Compute(inputs[0]); // 1.0003849827673186
+    /// </code>
+    /// </example>
+    /// 
     public class SequentialMinimalOptimizationRegression : ISupportVectorMachineLearning
     {
 
@@ -543,6 +579,7 @@ namespace Accord.MachineLearning.VectorMachines.Learning
         /// <summary>
         ///   Analytically solves the optimization problem for two Lagrange multipliers.
         /// </summary>
+        /// 
         private bool takeStep(int i1, int i2)
         {
             if (i1 == i2) return false;
