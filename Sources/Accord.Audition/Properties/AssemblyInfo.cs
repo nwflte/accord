21bddcce1ecf5c813eb2ec8f451876dd90051d09
cesarsouza <cesarsouza@0b1c26a9-55f7-2bfe-8466-e4ca5f927057>
2013-02-23 16:19:18 +0000

    Updating copyright information for Accord.Audition classes, updating Audition project.

--- Sources/Accord.Audition/Properties/AssemblyInfo.cs
+++ Sources/Accord.Audition/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Accord.NET")]
 [assembly: AssemblyProduct("Accord.Audition")]
-[assembly: AssemblyCopyright("Copyright © César Souza 2009-2012")]
+[assembly: AssemblyCopyright("Copyright © César Souza, 2009-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,8 +28,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("2.8.1.0")]
-[assembly: AssemblyFileVersionAttribute("2.8.1.0")]
+[assembly: AssemblyVersion("2.8.2.0")]
+[assembly: AssemblyFileVersionAttribute("2.8.2.0")]
 
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Accord.Audition.Beat")]
