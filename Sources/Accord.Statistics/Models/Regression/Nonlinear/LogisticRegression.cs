23d2ad63e2598055fccab4047745e4ffe332f95b
cesarsouza <cesarsouza@0b1c26a9-55f7-2bfe-8466-e4ca5f927057>
2013-02-23 15:32:26 +0000

    Including (experimental) support for generalized linear models (Fixes issue 36).

--- Sources/Accord.Statistics/Models/Regression/Nonlinear/LogisticRegression.cs
+++ Sources/Accord.Statistics/Models/Regression/Nonlinear/LogisticRegression.cs
@@ -2,7 +2,7 @@
 // The Accord.NET Framework
 // http://accord.googlecode.com
 //
-// Copyright © César Souza, 2009-2012
+// Copyright © César Souza, 2009-2013
 // cesarsouza at gmail.com
 //
 //    This library is free software; you can redistribute it and/or
@@ -26,6 +26,7 @@ namespace Accord.Statistics.Models.Regression
     using System.Linq;
     using Accord.Statistics.Testing;
     using AForge;
+    using Accord.Statistics.Links;
 
     /// <summary>
     ///   Binary Logistic Regression.
@@ -134,17 +135,9 @@ namespace Accord.Statistics.Models.Regression
     /// </example>
     /// 
     [Serializable]
-    public class LogisticRegression : ICloneable
+    public class LogisticRegression : GeneralizedLinearRegression
     {
 
-        private double[] coefficients;
-        private double[] standardErrors;
-
-
-        //---------------------------------------------
-
-
-        #region Constructor
         /// <summary>
         ///   Creates a new Logistic Regression Model.
         /// </summary>
@@ -152,10 +145,7 @@ namespace Accord.Statistics.Models.Regression
         /// <param name="inputs">The number of input variables for the model.</param>
         /// 
         public LogisticRegression(int inputs)
-        {
-            this.coefficients = new double[inputs + 1];
-            this.standardErrors = new double[inputs + 1];
-        }
+            : base(new LogitLinkFunction(), inputs) { }
 
         /// <summary>
         ///   Creates a new Logistic Regression Model.
@@ -165,106 +155,7 @@ namespace Accord.Statistics.Models.Regression
         /// <param name="intercept">The starting intercept value. Default is 0.</param>
         /// 
         public LogisticRegression(int inputs, double intercept)
-            : this(inputs)
-        {
-            this.coefficients[0] = intercept;
-        }
-        #endregion
-
-
-        //---------------------------------------------
-
-
-        #region Properties
-        /// <summary>
-        ///   Gets the coefficient vector, in which the
-        ///   first value is always the intercept value.
-        /// </summary>
-        /// 
-        public double[] Coefficients
-        {
-            get { return coefficients; }
-        }
-
-        /// <summary>
-        ///   Gets the standard errors associated with each
-        ///   cofficient during the model estimation phase.
-        /// </summary>
-        /// 
-        public double[] StandardErrors
-        {
-            get { return standardErrors; }
-        }
-
-        /// <summary>
-        ///   Gets the number of inputs handled by this model.
-        /// </summary>
-        /// 
-        public int Inputs
-        {
-            get { return coefficients.Length - 1; }
-        }
-        #endregion
-
-
-        //---------------------------------------------
-
-
-        #region Public Methods
-        /// <summary>
-        ///   Computes the model output for the given input vector.
-        /// </summary>
-        /// 
-        /// <param name="input">The input vector.</param>
-        /// <returns>The output value.</returns>
-        /// 
-        public double Compute(double[] input)
-        {
-            double logit = coefficients[0];
-
-            for (int i = 1; i < coefficients.Length; i++)
-                logit += input[i - 1] * coefficients[i];
-
-            return Logistic(logit);
-        }
-
-        /// <summary>
-        ///   Computes the model output for each of the given input vectors.
-        /// </summary>
-        /// 
-        /// <param name="input">The array of input vectors.</param>
-        /// <returns>The array of output values.</returns>
-        /// 
-        public double[] Compute(double[][] input)
-        {
-            double[] output = new double[input.Length];
-
-            for (int i = 0; i < input.Length; i++)
-                output[i] = Compute(input[i]);
-
-            return output;
-        }
-
-
-        /// <summary>
-        ///   Gets the Odds Ratio for a given coefficient.
-        /// </summary>
-        /// <remarks>
-        ///   The odds ratio can be computed raising euler's number
-        ///   (e ~~ 2.71) to the power of the associated coefficient.
-        /// </remarks>
-        /// <param name="index">
-        ///   The coefficient's index. The first value
-        ///   (at zero index) is the intercept value.
-        /// </param>
-        /// <returns>
-        ///   The Odds Ratio for the given coefficient.
-        /// </returns>
-        /// 
-        public double GetOddsRatio(int index)
-        {
-            return Math.Exp(coefficients[index]);
-        }
+            : base(new LogitLinkFunction(), inputs, intercept) { }
 
         /// <summary>
         ///   Gets the 95% confidence interval for the
@@ -278,8 +169,8 @@ namespace Accord.Statistics.Models.Regression
         /// 
         public DoubleRange GetConfidenceInterval(int index)
         {
-            double coeff = coefficients[index];
-            double error = standardErrors[index];
+            double coeff = Coefficients[index];
+            double error = StandardErrors[index];
 
             double upper = coeff + 1.9599 * error;
             double lower = coeff - 1.9599 * error;
@@ -290,15 +181,12 @@ namespace Accord.Statistics.Models.Regression
         }
 
         /// <summary>
-        ///   Gets the Wald Test for a given coefficient.
+        ///   Gets the Odds Ratio for a given coefficient.
         /// </summary>
         /// 
         /// <remarks>
-        ///   The Wald statistical test is a test for a model parameter in which
-        ///   the estimated parameter θ is compared with another proposed parameter
-        ///   under the assumption that the difference between them will be approximately
-        ///   normal. There are several problems with the use of the Wald test. Please
-        ///   take a look on substitute tests based on the log-likelihood if possible.
+        ///   The odds ratio can be computed raising euler's number
+        ///   (e ~~ 2.71) to the power of the associated coefficient.
         /// </remarks>
         /// 
         /// <param name="index">
@@ -306,146 +194,13 @@ namespace Accord.Statistics.Models.Regression
         ///   (at zero index) is the intercept value.
         /// </param>
         /// 
-        public WaldTest GetWaldTest(int index)
-        {
-            return new WaldTest(coefficients[index], 0.0, standardErrors[index]);
-        }
-
-
-        /// <summary>
-        ///   Gets the Log-Likelihood for the model.
-        /// </summary>
-        /// 
-        /// <param name="input">A set of input data.</param>
-        /// <param name="output">A set of output data.</param>
         /// <returns>
-        ///   The Log-Likelihood (a measure of performance) of
-        ///   the model calculated over the given data sets.
+        ///   The Odds Ratio for the given coefficient.
         /// </returns>
         /// 
-        public double GetLogLikelihood(double[][] input, double[] output)
+        public double GetOddsRatio(int index)
         {
-            double sum = 0;
-
-            for (int i = 0; i < input.Length; i++)
-            {
-                double actualOutput = Compute(input[i]);
-                double expectedOutput = output[i];
-
-                if (actualOutput != 0)
-                    sum += expectedOutput * Math.Log(actualOutput);
-
-                if (actualOutput != 1)
-                    sum += (1 - expectedOutput) * Math.Log(1 - actualOutput);
-
-                System.Diagnostics.Debug.Assert(!Double.IsNaN(sum));
-            }
-
-            return sum;
+            return Math.Exp(Coefficients[index]);
         }
-
-        /// <summary>
-        ///   Gets the Deviance for the model.
-        /// </summary>
-        /// 
-        /// <remarks>
-        ///   The deviance is defined as -2*Log-Likelihood.
-        /// </remarks>
-        /// 
-        /// <param name="input">A set of input data.</param>
-        /// <param name="output">A set of output data.</param>
-        /// <returns>
-        ///   The deviance (a measure of performance) of the model
-        ///   calculated over the given data sets.
-        /// </returns>
-        /// 
-        public double GetDeviance(double[][] input, double[] output)
-        {
-            return -2.0 * GetLogLikelihood(input, output);
-        }
-
-        /// <summary>
-        ///   Gets the Log-Likelihood Ratio between two models.
-        /// </summary>
-        /// 
-        /// <remarks>
-        ///   The Log-Likelihood ratio is defined as 2*(LL - LL0).
-        /// </remarks>
-        /// 
-        /// <param name="input">A set of input data.</param>
-        /// <param name="output">A set of output data.</param>
-        /// <param name="regression">Another Logistic Regression model.</param>
-        /// 
-        /// <returns>The Log-Likelihood ratio (a measure of performance
-        /// between two models) calculated over the given data sets.</returns>
-        /// 
-        public double GetLogLikelihoodRatio(double[][] input, double[] output, LogisticRegression regression)
-        {
-            return 2.0 * (this.GetLogLikelihood(input, output) - regression.GetLogLikelihood(input, output));
-        }
-
-
-        /// <summary>
-        ///   The likelihood ratio test of the overall model, also called the model chi-square test.
-        /// </summary>
-        /// 
-        /// <param name="input">A set of input data.</param>
-        /// <param name="output">A set of output data.</param>
-        /// 
-        /// <remarks>
-        ///   <para>
-        ///   The Chi-square test, also called the likelihood ratio test or the log-likelihood test
-        ///   is based on the deviance of the model (-2*log-likelihood). The log-likelihood ratio test 
-        ///   indicates whether there is evidence of the need to move from a simpler model to a more
-        ///   complicated one (where the simpler model is nested within the complicated one).</para>
-        ///   <para>
-        ///   The difference between the log-likelihood ratios for the researcher's model and a
-        ///   simpler model is often called the "model chi-square".</para>
-        /// </remarks>
-        /// 
-        public ChiSquareTest ChiSquare(double[][] input, double[] output)
-        {
-            double y0 = output.Count(y => y == 0.0);
-            double y1 = output.Length - y0;
-
-            LogisticRegression regression = new LogisticRegression(Inputs, Math.Log(y1 / y0));
-
-            double ratio = GetLogLikelihoodRatio(input, output, regression);
-            return new ChiSquareTest(ratio, coefficients.Length - 1);
-        }
-
-
-
-        /// <summary>
-        ///   Creates a new LogisticRegression that is a copy of the current instance.
-        /// </summary>
-        /// 
-        public object Clone()
-        {
-            var regression = new LogisticRegression(coefficients.Length);
-            regression.coefficients = (double[])this.coefficients.Clone();
-            regression.standardErrors = (double[])this.standardErrors.Clone();
-            return regression;
-        }
-        #endregion
-
-
-        //---------------------------------------------
-
-
-        #region Static Methods
-        /// <summary>
-        ///   The Logistic function.
-        /// </summary>
-        /// 
-        /// <param name="value">The logit parameter.</param>
-        /// 
-        public static double Logistic(double value)
-        {
-            return 1.0 / (1.0 + System.Math.Exp(-value));
-        }
-        #endregion
-
     }
-
 }
