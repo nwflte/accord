60f0fc15e8c96ff1480c685522fd40492be92a13
cesarsouza <cesarsouza@0b1c26a9-55f7-2bfe-8466-e4ca5f927057>
2013-07-26 02:32:47 +0000

    Adding examples for Codification and Normalization; updates issue #58.

--- trunk/Sources/Accord.Statistics/Filters/Codification.cs
+++ trunk/Sources/Accord.Statistics/Filters/Codification.cs
@@ -32,10 +32,141 @@ namespace Accord.Statistics.Filters
     /// </summary>
     /// 
     /// <remarks>
+    /// <para>
     ///   The codification filter performs an integer codification of classes in
     ///   given in a string form. An unique integer identifier will be assigned
-    ///   for each of the string classes.
+    ///   for each of the string classes.</para>
     /// </remarks>
+    /// 
+    /// <example>
+    /// <para>
+    ///   When handling data tables, often there will be cases in which a single
+    ///   table contains both numerical variables and categorical data in the form
+    ///   of text labels. Since most machine learning and statistics algorithms
+    ///   expect their data to be numeric, the codification filter can be used
+    ///   to create mappings between text labels and discrete symbols.</para>
+    ///   
+    /// <code>
+    ///   // Suppose we have a data table relating the age of
+    ///   // a person and its categorical classification, as 
+    ///   // in "child", "adult" or "elder".
+    ///   
+    ///   // The Codification filter is able to extract those
+    ///   // string labels and transform them into discrete
+    ///   // symbols, assigning integer labels to each of them
+    ///   // such as "child" = 0, "adult" = 1, and "elder" = 3.
+    ///   
+    ///   // Create the aforementioned sample table
+    ///   DataTable table = new DataTable("Sample data");
+    ///   table.Columns.Add("Age", typeof(int));
+    ///   table.Columns.Add("Label", typeof(string));
+    ///   
+    ///   //            age   label
+    ///   table.Rows.Add(10, "child");
+    ///   table.Rows.Add(07, "child");
+    ///   table.Rows.Add(04, "child");
+    ///   table.Rows.Add(21, "adult");
+    ///   table.Rows.Add(27, "adult");
+    ///   table.Rows.Add(12, "child");
+    ///   table.Rows.Add(79, "elder");
+    ///   table.Rows.Add(40, "adult");
+    ///   table.Rows.Add(30, "adult");
+    ///   
+    ///   
+    ///   // Now, let's say we need to translate those text labels
+    ///   // into integer symbols. Let's use a Codification filter:
+    ///   
+    ///   Codification codebook = new Codification(table);
+    ///   
+    ///   
+    ///   // After that, we can use the codebook to "translate"
+    ///   // the text labels into discrete symbols, such as:
+    ///   
+    ///   int a = codebook.Translate("Label", "child"); // returns 0
+    ///   int b = codebook.Translate("Label", "adult"); // returns 1
+    ///   int c = codebook.Translate("Label", "elder"); // returns 2
+    ///   
+    ///   // We can also do the reverse:
+    ///   string labela = codebook.Translate("Label", 0); // returns "child"
+    ///   string labelb = codebook.Translate("Label", 1); // returns "adult"
+    ///   string labelc = codebook.Translate("Label", 2); // returns "elder"
+    /// </code>
+    /// 
+    /// <para>
+    ///   After we have created the codebook, we can use it to feed data with
+    ///   categorical variables to method which would otherwise not know how
+    ///   to handle text labels data. Continuing with our example, the next
+    ///   code section shows how to convert an entire data table into a numerical
+    ///   matrix. </para>
+    /// 
+    /// <code>
+    ///   // We can process an entire data table at once:
+    ///   DataTable result = codebook.Apply(table);
+    ///   
+    ///   // The resulting table can be transformed to jagged array:
+    ///   double[][] matrix = Matrix.ToArray(result);
+    ///   
+    ///   // and the resulting matrix will be given by
+    ///   // new double[][] 
+    ///   // {
+    ///   //     new double[] { 10, 0 },
+    ///   //     new double[] {  7, 0 },
+    ///   //     new double[] {  4, 0 },
+    ///   //     new double[] { 21, 1 },
+    ///   //     new double[] { 27, 1 },
+    ///   //     new double[] { 12, 0 },
+    ///   //     new double[] { 79, 2 },
+    ///   //     new double[] { 40, 1 },
+    ///   //     new double[] { 30, 1 } 
+    ///   // };
+    ///   
+    ///   // PS: the string representation for the matrix above can be obtained by calling
+    ///   string str = matrix.ToString(CSharpJaggedMatrixFormatProvider.InvariantCulture);
+    /// </code>
+    /// 
+    /// <para>
+    ///   Finally, by expressing our data in terms of a simple numerical
+    ///   matrix we will be able to feed it to any machine learning algorithm.
+    ///   The following code section shows how to create a <see cref="Accord.Statistics.Kernels.Linear">
+    ///   linear</see> multi-class Support Vector Machine to classify ages into any
+    ///   of the previously considered text labels ("child", "adult" or "elder").</para>
+    /// 
+    /// <code>
+    ///   // Now we will be able to feed this matrix to any machine learning
+    ///   // algorithm without having to worry about text labels in our data:
+    ///   
+    ///   // Use the first column as input and the second column a output:
+    ///   
+    ///   double[][] inputs = matrix.GetColumns(0);      // Age column
+    ///   int[] outputs = matrix.GetColumn(1).ToInt32(); // Label column
+    ///   
+    ///   
+    ///   // Create a multi-class SVM for one input (Age) and three classes (Label)
+    ///   var machine = new MulticlassSupportVectorMachine(inputs: 1, classes: 3);
+    ///   
+    ///   // Create a Multi-class learning algorithm for the machine
+    ///   var teacher = new MulticlassSupportVectorLearning(machine, inputs, outputs);
+    ///   
+    ///   // Configure the learning algorithm to use SMO to train the
+    ///   //  underlying SVMs in each of the binary class subproblems.
+    ///   teacher.Algorithm = (svm, classInputs, classOutputs, i, j) =>
+    ///       new SequentialMinimalOptimization(svm, classInputs, classOutputs);
+    ///   
+    ///   // Run the learning algorithm
+    ///   double error = teacher.Run(); // error will be zero
+    ///   
+    ///   
+    ///   // After we have learned the machine, we can use it to classify
+    ///   // new data points, and use the codebook to translate the machine
+    ///   // outputs to the original text labels:
+    ///   
+    ///   string result1 = codebook.Translate("Label", machine.Compute(10)); // child
+    ///   string result2 = codebook.Translate("Label", machine.Compute(40)); // adult
+    ///   string result3 = codebook.Translate("Label", machine.Compute(70)); // elder
+    /// </code>
+    /// </example>
+    /// 
+    /// <seealso cref="Normalization"/>
     /// 
     [Serializable]
     public class Codification : BaseFilter<Codification.Options>, IAutoConfigurableFilter
@@ -67,39 +198,7 @@ namespace Accord.Statistics.Filters
             this.Detect(data, columns);
         }
 
-        /// <summary>
-        ///   Gets options, including mappings and dictionaries
-        ///   associated with a given variable (data column).
-        /// </summary>
-        /// 
-        /// <param name="columnName">The name of the variable.</param>
-        /// 
-        /// <returns>
-        ///   An <see cref="Options"/> object listing the main
-        ///   properties, such as number of possible symbols and
-        ///   symbol-value mapping for the given column.</returns>
-        /// 
-        public Options this[string columnName]
-        {
-            get { return Columns[columnName]; }
-        }
-
-        /// <summary>
-        ///   Gets options, including mappings and dictionaries
-        ///   associated with a given variable (data column).
-        /// </summary>
-        /// 
-        /// <param name="index">The column's index for the variable.</param>
-        /// 
-        /// <returns>
-        ///   An <see cref="Options"/> object listing the main
-        ///   properties, such as number of possible symbols and
-        ///   symbol-value mapping for the given column.</returns>
-        /// 
-        public Options this[int index]
-        {
-            get { return Columns[index]; }
-        }
+ 
 
         /// <summary>
         ///   Translates a value of a given variable
